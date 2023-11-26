24dd772d3da5e2c820be19adab007156a5ba1f93
prateekmishra1009 <prateekmishra10007@gmail.com>
2023-11-26 12:02:13 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from './Card';
+import Header from './Header';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [count,setCount]=useState(0)
+  const [timerRunning, setTimerRunning] = useState(false);
+  
+
+  const startTimer = () => {
+    setTimerRunning(true);
+  };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (timerRunning) {
+      intervalId = setInterval(() => {
+        setCount(prevCount => prevCount + 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [timerRunning]);
+
+
+  
+
   return (
     <div className="App">
-     <h1>Hello world</h1>
+    <Header />
+      <Card title="CARD" />
+     <p>my count is {count}</p>
+     <button onClick={()=>startTimer()}> Increment the timer</button>
     </div>
   );
 }
